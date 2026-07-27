02de9ca400d39f11e881025d5ad9173cb2484d32
giftonj <giftonnjoroge@gmail.com>
2026-07-27 15:43:38 +0300

    identify the framework and the language used in the root directory

--- Readme Generator/Readers/ProjectReader.cs
+++ Readme Generator/Readers/ProjectReader.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Readme_Generator.Models;
 
 namespace Readme_Generator.Readers;
@@ -13,6 +14,71 @@ public class ProjectReader
 
             string[] lines = File.ReadAllLines(r);
             Console.WriteLine($"Reading: {file.Name}");
+            
+           
+            var extension = Path.GetExtension(file.Name);
+            
+            if (extension == ".cs" || files.Any(f => f.Name.Contains(".csproj")))
+            {
+                Console.WriteLine("This is a C# project .");
+            }
+            else if (extension == ".py" || extension == ".pyproj" || file.Name == "pyproject.toml" )
+            {
+                Console.WriteLine("This is a Python project .");
+            }
+            else if (extension == ".md")
+            {
+                Console.WriteLine("This is a Markdown project .");
+            }
+            else if(extension == ".json" && file.Name == "package.json")
+            {
+                var document = JsonDocument.Parse(file.Name);
+                if (document.RootElement.TryGetProperty("dependencies", out JsonElement dependencies))
+                {
+                    Console.WriteLine("This is a Node.js based project .");
+                    
+                    if (dependencies.TryGetProperty("react", out JsonElement react))
+                    {
+                        Console.WriteLine($"React: {react.GetString()}");
+                    }
+                    else if (dependencies.TryGetProperty("vue", out JsonElement vue))
+                    {
+                        Console.WriteLine($"Vue:  {vue.GetString()}");
+                    }
+                    else if (dependencies.TryGetProperty("express", out JsonElement express))
+                    {
+                        Console.WriteLine($"Express:   {express.GetString()}");
+                    }
+                    else if (dependencies.TryGetProperty("next", out JsonElement next))
+                    {
+                        Console.WriteLine($"Next.js: {next.GetString()}");
+                    }
+                    else if (dependencies.TryGetProperty("@nestjs/core", out JsonElement core))
+                    {
+                        Console.WriteLine($"Nest.js: {core.GetString()}");
+                    }
+                }
+            }
+            else if (extension == ".txt")
+            {
+                continue;
+            }
+            else if (extension == ".toml" && file.Name == "Cargo.toml")
+            {
+                Console.WriteLine("This is a Rust project .");
+            }
+            else if (file.Name == "go.mod" || extension == ".mod")
+            {
+                Console.WriteLine("This is a Go project .");
+            }
+            else if (file.Name == "pom.xml" || extension == ".java")
+            {
+                Console.WriteLine("This is a Java project .");
+            }
+            else if (file.Name == "build.gradle" || extension == ".gradle")
+            {
+                Console.WriteLine("This is a Kotlin project .");
+            }
             
             projectFiles.Add(new ProjectFile
             {
